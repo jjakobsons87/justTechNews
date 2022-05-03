2aed51712a02cd73301ace8599005adfbad722aa
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-02 22:44:31 -0400

    add route functionality

--- models/User.js
+++ models/User.js
@@ -9,13 +9,13 @@ User.init(
     {
         // define an id column 
         id: {
-            // use the special Sequelize DataTypes object proviced what type of data it is 
+            // use the special Sequelize DataTypes object provided what type of data it is 
             type: DataTypes.INTEGER,
             // this is the equivalent of SQL's `NOT NULL` option
             allowNull: false,
-            // instruct that this is the primark key
+            // instruct that this is the primary key
             primaryKey: true,
-            // turn on auto invrememnt 
+            // turn on auto increment 
             autoIncrement: true
         },
         // define a username column 
@@ -27,9 +27,9 @@ User.init(
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            // there cannot be any duplicate email valuse in this table 
+            // there cannot be any duplicate email values in this table 
             unique: true, 
-            // if allowNull is set to false, we can run our data trhough validators before creating the table data 
+            // if allowNull is set to false, we can run our data through validators before creating the table data 
             validate: {
                 isEmail: true
             }
@@ -39,7 +39,7 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                // this means the password must be at least four charaters long 
+                // this means the password must be at least four characters long 
                 len: [4]
             }
         }
@@ -52,7 +52,7 @@ User.init(
         timestamps: false,
         // don't pluralize name of database table 
         freezeTableName: true,
-        // use underscores instaead of camel-casing (i.e. `comment_text` and not `commentText`
+        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`
         underscored: true,
         // make it so our model name stays lowercase in the database 
         modelName: 'user'
