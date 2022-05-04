6ce3413e48cd8e652de54ec06958c913fdccf633
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-04 18:45:57 -0400

    add post/user associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations 
+// linking the user id to the post - one user can have many posts 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// linking the post to the user id - the post can belong to one user, not many
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
