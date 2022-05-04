824863c6ee3842687b3855941d0cafa99a46bc6a
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-04 19:39:37 -0400

    update routes and implement get all posts

--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@ const User = require('./User');
 const Post = require('./Post');
 
 // create associations 
-// linking the user id to the post - one user can have many posts 
+// linking the user id to the post - one user can have many posts: hasMany
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
-// linking the post to the user id - the post can belong to one user, not many
+// linking the post to the user id - the post can belong to one user, not many: belongsTo
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
