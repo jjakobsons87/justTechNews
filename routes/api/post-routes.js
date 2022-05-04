2a3828cce560a75bbb47ca1845743dcb2c04e0d0
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-04 19:48:41 -0400

    add get a single post function

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
 
-// get all users 
+// get all posts
 router.get('/', (req, res) => {
     console.log("======================");
     Post.findAll({
@@ -14,6 +14,33 @@ router.get('/', (req, res) => {
         ]
     })
     .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// get a single post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No Post found with this id' });
+            ReadableStreamDefaultController;
+        }
+        res.json(dbPostData);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
