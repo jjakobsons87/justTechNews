8480f1cc030dcbc374358cf7eaea211a6fadc459
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-07 23:03:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
