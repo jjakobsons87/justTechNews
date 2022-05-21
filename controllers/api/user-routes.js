41727f2ecd3cb922da1200fc390bdce42f0e9900
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-21 15:08:36 -0400

    update login logic

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -65,11 +65,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_is = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
 });
 
 router.post('/login', (req, res) => {
@@ -92,8 +96,15 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
-        // if password matches, you are now logged in 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+          // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+          // if password matches, you are now logged in
+            res.json({ user: dbUserData, message: "You are now logged in!" });
+        });
     });
 });
 
--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
-
-
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.set("view engine", "handlebars");
+
+const sequelize = require("./config/connection");
+
+const hbs = exphbs.create({});
+
 app.engine("handlebars", hbs.engine);
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const routes = require("./controllers");
+
+const session = require('express-session');
+
+const SequelizeStore = require ('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 // turn on routes 
 app.use(routes);
