63b869fc184de7c0e9ca2f507dcaa1f103e2d182
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-21 11:53:39 -0400

    update login functionality

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,4 @@
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
@@ -6,7 +6,7 @@ function signupFormHandler(event) {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && email && password) {
-        fetch('/api/users', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username, 
@@ -14,8 +14,42 @@ function signupFormHandler(event) {
                 password
             }),
             headers: { 'Content-type': 'application/json' }
-        }).then((response) => {console.log(response)})
+        });
+
+        // check the response status 
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
+// login 
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if ( email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email, 
+                password
+            }),
+            headers: { 'Content-type': 'application/json' }
+        });
+
+        // check the response status 
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
