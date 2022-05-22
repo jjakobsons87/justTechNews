1c832f1712adce48f3e07c008e592d208415b666
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-22 12:00:10 -0400

    update for snapshot

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -80,16 +80,18 @@ router.get('/:id', (req, res) => {
 // Create a post 
 router.post('/', (req, res) => {
     // expects Title, posturl, and user id in the call 
-    Post.create({
-        title: req.body.title,
-        post_url: req.body.post_url,
-        user_id: req.body.user_id
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    if (req.session) {
+        Post.create({
+            title: req.body.title,
+            post_url: req.body.post_url,
+            user_id: req.body.user_id
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 // PUT /api/posts/upvote 
--- models/Comment.js
+++ models/Comment.js
@@ -28,7 +28,6 @@ Comment.init(
         },
         post_id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
             references: {
                 model: 'post',
                 key: 'id'
